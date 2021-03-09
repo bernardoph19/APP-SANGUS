ff38148041de4bccb3e8392ddbb7ae9f4799d303
bernardo <bernardoph19@gmail.com>
2021-03-09 16:31:03 -0500

    cambios  nuevos

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(public loadingController: LoadingController) { }
+
+  ngOnInit() {
+  }
+
+  async presentLoading() {
+
+    const loading = await this.loadingController.create({
+      //cssClass: 'my-custom-class',
+      message: 'cargando',
+      duration: 2000
+    });
+
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+
+    console.log('Loading dismissed!');
+  }
+
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 5000,
+      message: 'Click the backdrop to dismiss early...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading',
+      backdropDismiss: true
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed with role:', role);
+  }
+
+}
