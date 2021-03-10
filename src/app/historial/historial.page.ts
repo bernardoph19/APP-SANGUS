fed8e4dfacbfe7017d6494fb6c6a86d5791b4701
bernardo <bernardoph19@gmail.com>
2021-03-10 17:02:21 -0500

    cambios nuevos

---
+++ src/app/historial/historial.page.ts
@@ -0,0 +1,73 @@
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-historial',
+  templateUrl: './historial.page.html',
+  styleUrls: ['./historial.page.scss'],
+})
+export class HistorialPage implements OnInit {
+
+  constructor(public alertController: AlertController) {}
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Filtro',
+      inputs: [
+
+        {
+          name: 'name1',
+          type: 'text',
+          cssClass:'bg-red-600',
+          placeholder: 'Placeholder 1',
+        },
+
+
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+
+
+        {
+          name: 'name8',
+          type: 'password',
+          placeholder: 'Advanced Attributes',
+          cssClass: 'specialClass',
+          attributes: {
+            maxlength: 4,
+            inputmode: 'decimal'
+          }
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'primary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+
+        },
+         {
+          cssClass:'btn',
+          text: 'aplicar',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  ngOnInit() {
+  }
+
+}
